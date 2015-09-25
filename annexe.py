94c9dcabc9374ed0e93837664137fbf79eb9ebb1
Luca De Feo <dfl@deagol>
2015-09-26 00:20:20 +0200

    Fixes to annexe generation

--- annexe.py
+++ annexe.py
@@ -6,11 +6,12 @@ with open('annexe-data.tex', 'w') as out:
     with open('bdd-decors.csv') as bdd:
         reader = csv.DictReader(bdd, delimiter=',', quotechar='"')
         for row in reader:
-            out.write(r'''\begin{figure}
-  \includegraphics[width=\textwidth]{%(num)s.jpg}
-  \caption{
-    \house{%(house)s} \name{%(name)s} \subject{%(subject)s} 
-    \type{%(type)s} \date{%(date)s} \room{%(room)s} \wall{%(wall)s}
-    \painter{%(painter)s} \location{%(location)s} \credits{%(credits)s}}
-\end{figure}
+            out.write(r'''
+  \includegraphics[width=\textwidth]{bdd-decors/%(num)s.jpg}
+
+  \house{%(house)s} \name{%(name)s} \subject{%(subject)s} 
+  \type{%(type)s} \date{%(date)s} \room{%(room)s} \wall{%(wall)s}
+  \painter{%(painter)s} \location{%(location)s} \credits{%(credits)s}
+
+\clearpage
 ''' % row)
