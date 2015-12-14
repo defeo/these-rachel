1e9d54ad4fac827775a95d4752525bbd76ad3631
Luca De Feo <defeo@pippin>
2015-12-14 23:07:42 +0100

    Python3 compat

--- bib-citeall.py
+++ bib-citeall.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
 import re
+import sys
 
 with open('bibliography-cites.tex', 'w') as out:
     regex = re.compile("@[a-zA-Z]+{(.*),$")
     for bib in ['antiques', 'modernes']:
-        with open('%s.bib' % bib) as db:
+        kwds = {'encoding': 'iso-8859-15'} if (sys.version_info[0] > 2) else {}
+        with open('%s.bib' % bib, **kwds) as db:
             keys = []
             for l in db:
                 m = regex.match(l)
